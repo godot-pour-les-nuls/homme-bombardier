81c8f5056198883dca737eaad41edfa9f61a77b7
Himyo <himyo97@gmail.com>
2020-12-05 18:38:12 +0100

    finished script chapter, add little movement script

--- characters/friendly/Player/Player.gd
+++ characters/friendly/Player/Player.gd
@@ -1,16 +1,14 @@
 extends KinematicBody2D
 
+var velocity : Vector2 = Vector2(0, 0) 
+var speed : int = 200
+var gravity: int = 800
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	velocity.x = 0
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= speed
+	if Input.is_action_pressed("move_right"):
+		velocity.x += speed
+	velocity = move_and_slide(velocity, Vector2.UP)
+	# velocity.y += gravity * delta
